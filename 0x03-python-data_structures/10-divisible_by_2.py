f3024af5f3d2dfe07dd4a4f646d7da724e342f35
gregkillinwtf <ayomidegreg@gmail.com>
2022-07-04 08:09:13 -0700

    PDS

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    multiples_of_two = my_list[:]
-    for i in range(len(multiples_of_two)):
-        if multiples_of_two[i] % 2 == 0:
-            multiples_of_two[i] = True
+    new_list = list(my_list)
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list[i] = True
         else:
-            multiples_of_two[i] = False
-            return multiples_of_two
+            new_list[i] = False
+    return new_list            
